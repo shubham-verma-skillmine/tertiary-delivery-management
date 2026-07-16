fa7215c573e0efb66a1440ef92aab198259f0b18
shubham-verma-skillmine <shubham.verma@skill-mine.com>
2026-07-16 11:22:20 +0530

    fix for production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/tp/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
